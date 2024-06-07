f903a435bd8065ba38ca0c3e6e3bbd0d378f74cf
Agga <agga@Aggas-MacBook-Air.local>
2024-06-07 21:08:42 +0700

    [add] navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import {Component, onMounted, useRef, useState, xml} from "@odoo/owl";
+import {Task} from "./Task";
+import {useStore} from "../Store";
+// -------------------------------------------------------------------------
+// Root Component
+// -------------------------------------------------------------------------
+export default class Navbar extends Component {
+    static template = xml/* xml */`
+      `;
+}

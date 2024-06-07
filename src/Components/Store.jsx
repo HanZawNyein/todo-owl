85fa7973e4a974ec52d7e7baa7c0b5d3216872f2
Agga <agga@Aggas-MacBook-Air.local>
2024-06-07 22:35:14 +0700

    Updates

--- src/Components/Store.jsx
+++
@@ -1,9 +0,0 @@
-import {useEnv, useState} from "@odoo/owl";
-
-// -------------------------------------------------------------------------
-// Store
-// -------------------------------------------------------------------------
-export function useStore() {
-    const env = useEnv();
-    return useState(env.store);
-}
